985ef696089a06a8496d1e74baadbc71e7210695
AndreasNel <nel.andreas1@gmail.com>
2018-10-14 21:40:51 +0200

    added use of validation set

--- lstm.py
+++ lstm.py
@@ -6,12 +6,13 @@ from keras import utils
 from DataManager import DataManager
 
 print("Loading training data...")
-dm = DataManager()
+dm = DataManager(random_state=0)
 training_data, training_labels = dm.loadTrainingData()
 testing_data, testing_labels = dm.loadTestingData()
+validation_data, validation_labels = dm.loadValidationData()
 
 print('Loaded shapes')
-for i in training_data, training_labels, testing_data, testing_labels:
+for i in training_data, training_labels, testing_data, testing_labels, validation_data, validation_labels:
     print(i.shape)
 
 input_shape = tuple(training_data.shape[1:])
@@ -22,17 +23,17 @@ print("num_classes: {}".format(num_classes))
 # Convert to categorical classes
 training_labels = utils.to_categorical(training_labels, num_classes)
 testing_labels = utils.to_categorical(testing_labels, num_classes)
+validation_labels = utils.to_categorical(validation_labels, num_classes)
 
 # Build the model
 print("Building Model...")
 model = Sequential()
-model.add(BatchNormalization(input_shape=input_shape))
+# model.add(BatchNormalization(input_shape=input_shape))
 print("Adding layer 0")
-model.add(LSTM(105, activation='sigmoid', return_sequences=True))
-model.add(LSTM(105, activation='sigmoid', return_sequences=True))
-# model.add(LSTM(105, activation='sigmoid', return_sequences=True))
-# print("Adding hidden layer")
-# model.add(LSTM(105, activation='sigmoid', return_sequences=True))
+model.add(Dense(105, input_shape=input_shape, activation='relu'))
+model.add(BatchNormalization())
+model.add(LSTM(105, activation='relu', return_sequences=True))
+model.add(BatchNormalization())
 model.add(Flatten())
 print("Adding output layer")
 model.add(Dense(num_classes, activation='softmax'))
@@ -40,8 +41,8 @@ print("Compiling the model")
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 print("Training the model")
-history = model.fit(training_data, training_labels, batch_size=320, epochs=30, verbose=1)
+history = model.fit(training_data, training_labels, validation_data=(validation_data, validation_labels), batch_size=160, epochs=30, verbose=1)
 # Test the model with the testing data.
-print("Testing the model")
-score = model.evaluate(testing_data, testing_labels)
+# print("Testing the model")
+# score = model.evaluate(testing_data, testing_labels)
 
